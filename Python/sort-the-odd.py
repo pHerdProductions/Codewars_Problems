dec5c0be07fe727f16afc3820dc7f3d45c0d016f
Ralph <64453143+pHerdProductions@users.noreply.github.com>
2023-04-14 00:38:43 -0400

    Update sort-the-odd.py

--- Python/sort-the-odd.py
+++ Python/sort-the-odd.py
@@ -1,36 +1,8 @@
 # Function to, for the array/list, sort the odd numbers, keeping the positions of the even numbers
 def sort_array(arr):
   
-  # If the given array is empty, we can simply return an empty array
-  if not arr: return []
-
-  odds = [] # Variable to hold the list of odd numbers
-  pos = []  # Variable to hold the list of indices where odd numbers are found
+  # Create an array for every odd number in the given arr, and sort from largest to smallest
+  odds = sorted((x for x in arr if x%2 != 0), reverse = True)
   
-  # Iterating through the array
-  for i, n in enumerate(arr):
-    
-    # If the number is odd
-    if n % 2 == 1:
-      
-      # Append the number to our 'odds' array, and insert the index number into our 'pos' array (so we don't have to call reverse() later)
-      odds.append(n)
-      pos.insert(0, i)
-
-  odds.sort(reverse=True) # Sort the array of odd numbers from largest to smallest. We do this because, like 'pos' array, we will be using .pop()
-
-  # Iterate for the length of the initial array
-  for i in range(len(arr)):
-    
-    # If 'pos' is not empty and the last element is equal to i (our current position in the array)
-    if pos and i == pos[-1]:
-      
-      # Change that value to the last value of odds with pop() (which is the smallest number). Also pop() the last element out of 'pos'
-      arr[i] = odds.pop()
-      pos.pop()
-      
-      # If our 'pos' array is empty, break out of the loop. Could use 'odds' variable too
-      if not pos: break
-
-  # Return the new array!
-  return arr
+  # Return an array where, iterating through arr, we keep the evens in place, but if it's odd, pop() from our sorted 'odds' array instead
+  return [x if x%2 == 0 else odds.pop() for x in arr]
